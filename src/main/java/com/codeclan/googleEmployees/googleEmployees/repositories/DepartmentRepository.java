83e4126bbf94e86753d47ab435c7950939ffe4e5
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-04-09 10:33:59 +0100

    All the repositories set up

--- src/main/java/com/codeclan/googleEmployees/googleEmployees/repositories/DepartmentRepository.java
+++ src/main/java/com/codeclan/googleEmployees/googleEmployees/repositories/DepartmentRepository.java
@@ -1,4 +1,11 @@
 package com.codeclan.googleEmployees.googleEmployees.repositories;
 
-public interface DepartmentRepository {
+import com.codeclan.googleEmployees.googleEmployees.models.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+
 }
--- src/main/java/com/codeclan/googleEmployees/googleEmployees/repositories/ProjectRepository.java
+++ src/main/java/com/codeclan/googleEmployees/googleEmployees/repositories/ProjectRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.googleEmployees.googleEmployees.repositories;
 
-public interface ProjectRepository {
+import com.codeclan.googleEmployees.googleEmployees.models.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
