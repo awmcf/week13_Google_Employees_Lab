143c6419077c31e4598947db580e31bfe72566c3
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-04-09 10:29:43 +0100

    Project model and Department model set up

--- src/main/java/com/codeclan/googleEmployees/googleEmployees/models/Department.java
+++ src/main/java/com/codeclan/googleEmployees/googleEmployees/models/Department.java
@@ -1,4 +1,44 @@
 package com.codeclan.googleEmployees.googleEmployees.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
+
+    private Long id;
+    private String name;
+    private List<Employee> employees;
+
+    public Department(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public List<Employee> getEmployee() {
+        return this.employees;
+    }
+
+    public void setEmployees(List<Employee> employees ) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        this.employees.add(employee);
+    }
 }
--- src/main/java/com/codeclan/googleEmployees/googleEmployees/models/Project.java
+++ src/main/java/com/codeclan/googleEmployees/googleEmployees/models/Project.java
@@ -1,4 +1,50 @@
 package com.codeclan.googleEmployees.googleEmployees.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
+
+    private Long id;
+    private String name;
+    private int days;
+    private List<Employee> employees;
+
+    public Project(String name, int days) {
+        this.name = name;
+        this.days = days;
+        this.employees = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public int getDays() {
+        return this.days;
+    }
+
+    public void setDays(int newDays) {
+        this.days = newDays;
+    }
+
+    public List<Employee> getEmployee() {
+       return employees;
+    }
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
 }
